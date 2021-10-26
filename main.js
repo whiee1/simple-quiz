4260631ebbe99b1035d0d6659d7f6ba023e1be3a
whiee1 <whiee__@hotmail.com>
2021-10-26 18:54:59 +0200

    added question array

--- main.js
+++ main.js
@@ -1,3 +1,96 @@
+let theQuestions = [
+  {
+     question: "1. What is the color of the sun?",
+     answers: {
+       a: "Yellow",
+       b: "Green",
+       c: "Blue"
+     },
+     correctAnswer: "a"
+  },
+  {
+   question: "2. What is the color of the sun?",
+   answers: {
+     a: "Yellow",
+     b: "Green",
+     c: "Blue"
+   },
+   correctAnswer: "a"
+  },
+  {
+   question: "3. What is the color of the sun?",
+   answers: {
+     a: "Yellow",
+     b: "Green",
+     c: "Blue"
+   },
+   correctAnswer: "a"
+  },
+  {
+   question: "4. What is the color of the sun?",
+   answers: {
+     a: "Yellow",
+     b: "Green",
+     c: "Blue"
+   },
+   correctAnswer: "a"
+  },
+  {
+   question: "5. What is the color of the sun?",
+   answers: {
+     a: "Yellow",
+     b: "Green",
+     c: "Blue"
+   },
+   correctAnswer: "a"
+  },
+  {
+   question: "6. What is the color of the sun?",
+   answers: {
+     a: "Yellow",
+     b: "Green",
+     c: "Blue"
+   },
+   correctAnswer: "a"
+  },
+  {
+   question: "7. What is the color of the sun?",
+   answers: {
+     a: "Yellow",
+     b: "Green",
+     c: "Blue"
+   },
+   correctAnswer: "a"
+  },
+  {
+   question: "8. What is the color of the sun?",
+   answers: {
+     a: "Yellow",
+     b: "Green",
+     c: "Blue"
+   },
+   correctAnswer: "a"
+  },
+  {
+   question: "9. What is the color of the sun?",
+   answers: {
+     a: "Yellow",
+     b: "Green",
+     c: "Blue"
+   },
+   correctAnswer: "a"
+  },
+  {
+   question: "10. What is the color of the sun?",
+   answers: {
+     a: "Yellow",
+     b: "Green",
+     c: "Blue"
+   },
+   correctAnswer: "a"
+  }
+ ];
+
 // Klicka på knapp för att ändra mode
 changeMode = () => {
   document.body.classList.toggle("darkMode");
